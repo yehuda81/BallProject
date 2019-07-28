8e11015a2c91e64105c1bc79aeb767d4b5d0375f
yehuda81 <44754603+yehuda81@users.noreply.github.com>
2019-07-28 11:27:02 +0300

    Add files via upload

---
+++ PopBall/PopBall/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace PopBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ballPB5 = new System.Windows.Forms.PictureBox();
+            this.ballPB4 = new System.Windows.Forms.PictureBox();
+            this.ballPB3 = new System.Windows.Forms.PictureBox();
+            this.ballPB2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.Location = new System.Drawing.Point(349, 337);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "POP BALL";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::PopBall.Properties.Resources.boom;
+            this.pictureBox1.Location = new System.Drawing.Point(3000, 3000);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ballPB5
+            // 
+            this.ballPB5.Image = global::PopBall.Properties.Resources.BasketBall;
+            this.ballPB5.Location = new System.Drawing.Point(3000, 3000);
+            this.ballPB5.Name = "ballPB5";
+            this.ballPB5.Size = new System.Drawing.Size(150, 150);
+            this.ballPB5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ballPB5.TabIndex = 5;
+            this.ballPB5.TabStop = false;
+            this.ballPB5.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ballPB5_MouseClick);
+            // 
+            // ballPB4
+            // 
+            this.ballPB4.Image = global::PopBall.Properties.Resources.BasketBall;
+            this.ballPB4.Location = new System.Drawing.Point(3000, 3000);
+            this.ballPB4.Name = "ballPB4";
+            this.ballPB4.Size = new System.Drawing.Size(150, 150);
+            this.ballPB4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ballPB4.TabIndex = 4;
+            this.ballPB4.TabStop = false;
+            this.ballPB4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ballPB4_MouseClick);
+            // 
+            // ballPB3
+            // 
+            this.ballPB3.Image = global::PopBall.Properties.Resources.BasketBall;
+            this.ballPB3.Location = new System.Drawing.Point(3000, 3000);
+            this.ballPB3.Name = "ballPB3";
+            this.ballPB3.Size = new System.Drawing.Size(150, 150);
+            this.ballPB3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ballPB3.TabIndex = 3;
+            this.ballPB3.TabStop = false;
+            this.ballPB3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ballPB3_MouseClick);
+            // 
+            // ballPB2
+            // 
+            this.ballPB2.Image = global::PopBall.Properties.Resources.BasketBall;
+            this.ballPB2.Location = new System.Drawing.Point(3000, 3000);
+            this.ballPB2.Name = "ballPB2";
+            this.ballPB2.Size = new System.Drawing.Size(150, 150);
+            this.ballPB2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ballPB2.TabIndex = 2;
+            this.ballPB2.TabStop = false;
+            this.ballPB2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ballPB2_MouseClick_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(383, 382);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Score: 0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button2.Location = new System.Drawing.Point(850, 300);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 44);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "New Game";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button2_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ballPB5);
+            this.Controls.Add(this.ballPB4);
+            this.Controls.Add(this.ballPB3);
+            this.Controls.Add(this.ballPB2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPB2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox ballPB2;
+        private System.Windows.Forms.PictureBox ballPB3;
+        private System.Windows.Forms.PictureBox ballPB4;
+        private System.Windows.Forms.PictureBox ballPB5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PopBall/PopBall/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PopBall
+{
+    public partial class Form1 : Form
+    {
+        List<PictureBox> balls = new List<PictureBox>();
+        int ballNumber1;
+        bool key;        
+        int ballBomb;
+        Label label2 = new Label();     
+        public Form1()
+        {
+            InitializeComponent();
+        }        
+        int sleepBall = 2;
+        int sizeX = 100;
+        int sizeY = 100;
+        Random randomX = new Random();
+        Random randomY = new Random();
+        private void Form1_Load(object sender, EventArgs e)
+        {            
+            button1.Hide();
+            label1.Hide();
+        }
+        private void StartGame()
+        {
+            new Task(() =>
+            {                
+                button1.BackColor = Color.Firebrick;
+                button1.Text = "Pop Ball";
+                button1.Location = new Point((Form1.ActiveForm.Width - 60) / 2, Form1.ActiveForm.Height - 160);
+                label1.Location = new Point((Form1.ActiveForm.Width - 20) / 2, Form1.ActiveForm.Height - 130);
+            }).Start();
+            button2.Hide();                    
+            button1.Show();
+            label1.Hide();
+            this.Controls.Add(label2);
+            label2.Hide();
+            key = true;
+        }
+
+        private void BallMove(int ballNumber)
+        { 
+                int dx = 1;
+                int dy = 1;
+            key = !key;
+            ballNumber1++;
+            while (balls[ballNumber] != null)
+            {
+                balls[ballNumber].Location = new Point(balls[ballNumber].Location.X + dx, balls[ballNumber].Location.Y + dy);
+                Thread.Sleep(sleepBall);
+                if (balls[ballNumber].Location.X == Form1.ActiveForm.Width - 150)
+                {
+                    dx = -dx;
+                }
+                if (balls[ballNumber].Location.Y == Form1.ActiveForm.Height - 150)
+                {
+                    dy = -dy;
+                }
+                if (balls[ballNumber].Location.X == 0)
+                {
+                    dx = -dx;
+                }
+                if (balls[ballNumber].Location.Y == 0)
+                {
+                    dy = -dy;
+                } 
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (ballBomb == 10)
+            {
+                button1.Text = "Pop Ball";
+                ballBomb = 0;
+                label1.Text = $"Score: {ballBomb}";                           
+            }
+            if (key)
+            {                
+                key = !key;
+                balls.Add(NewBall());                
+                {
+                    new Task(() =>
+                    {
+                        BallMove(ballNumber1);                        
+                    }).Start();
+                }
+            }
+        }
+        
+        private PictureBox NewBall()
+        {
+            
+                int a = randomX.Next(Form1.ActiveForm.Width - 150);
+                int b = randomY.Next(Form1.ActiveForm.Height - 150);
+                PictureBox ball = new PictureBox()
+                {
+                    Image = Image.FromFile("C:\\Users\\yudal\\source\\repos\\popBall\\PopBall\\Resources\\BasketBall.jpg"),
+                    Location = new Point(a, b),
+                    Size = new Size(sizeX, sizeY),
+                    SizeMode = PictureBoxSizeMode.Zoom
+                };
+                this.Controls.Add(ball);                
+                ball.Click += new EventHandler(ball_Click);
+           
+            return ball;
+        }
+
+        private void ball_Click(object sender, EventArgs e)
+        {
+            PictureBox picture = (PictureBox)sender;
+            picture.Image = Image.FromFile("C:\\Users\\yudal\\source\\repos\\popBall\\PopBall\\Resources\\Boom.jpg");
+            new Task(() =>
+            {
+                Thread.Sleep(50);
+                this.Controls.Remove(picture);
+            }).Start();
+            ballBomb++;
+            label1.Show();
+            if (ballBomb < 10)
+            {
+                label1.Text = $"Score: {ballBomb}";
+            }
+            else
+            {
+                label1.Text = $"Score: {ballBomb}";                
+                label2.Text = "You Win!";
+                label2.Size = new Size(150, 150);
+                label2.Font = new Font("Arial", 20, FontStyle.Bold);
+                label2.ForeColor = Color.Blue;
+                label2.Location = new Point(590, 100);                             
+                label2.Show();
+                label1.Hide();
+                button1.Hide();
+                foreach (PictureBox ball in balls)
+                {
+                    this.Controls.Remove(ball);
+                }               
+                button2.Show();
+            }
+            
+        }
+        
+        private void Ball_Mouse_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void ballPB_MouseClick(object sender, MouseEventArgs e)
+        {            
+            
+        }
+       
+
+        private void ballPB2_MouseClick_1(object sender, MouseEventArgs e)
+        {
+           
+        }
+        
+        private void ballPB3_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void ballPB4_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void ballPB5_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+        
+        private void Form1_MouseClick(Object sender, MouseEventArgs e)
+            
+        {
+            
+        }
+
+        private void button2_MouseClick(object sender, MouseEventArgs e)
+        {
+            StartGame();
+        }
+    }
+}
